b0d83bb416f764e02e65a016b916ca669a44bd92
Aidan Sullivan <asulliva@f1r2s19.codam.nl>
2020-02-08 17:35:40 +0100

    changed some thing in get_label

--- new_asm/parse/get_label.c
+++ new_asm/parse/get_label.c
@@ -6,7 +6,7 @@
 /*   By: asulliva <asulliva@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/06 16:29:14 by asulliva       #+#    #+#                */
-/*   Updated: 2020/02/07 19:36:14 by abumbier      ########   odam.nl         */
+/*   Updated: 2020/02/08 17:35:09 by asulliva      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -136,12 +136,12 @@ void		get_label(t_asm *data, char **line)
 	split = NULL;
 	label_syntax(line[0], data->lines);
 	split = ft_strsplit(line[0], LABEL_CHAR);
-	if (split[1])												// check also for a line to add if line[1] exists
+	if (split[1] || check_instruction(line[1]))												// check also for a line to add if line[1] exists
 	{
 		new = make_label(data, split[0], data->lines);
 		add_label(data, &new);
 		ft_strclr(line[0]);
-		line[0] = ft_strdup(split[1]);
+		line[0] = (check_instruction(line[1]) ? ft_strdup(line[1]) : ft_strdup(split[1]));
 		ft_printf("[%s][%d]\n", new->name, new->line);						// CMD X
 		parse_instruction(data, line);
 	}
