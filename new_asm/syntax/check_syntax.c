63e16c137c0800b6709fa23e29169b3c26001d1b
Arturs Bumbieris <abumbier@f1r2s21.codam.nl>
2020-02-08 17:20:35 +0100

    also files :|

---
+++ new_asm/syntax/check_syntax.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   check_syntax.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: abumbier <abumbier@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/12/09 18:06:37 by abumbier       #+#    #+#                */
+/*   Updated: 2020/02/06 19:47:40 by abumbier      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/asm.h"
+
+/*
+**	@desc	- main controller function for checking the syntax and arguments
+**	@param	- t_parts file, tokenized file
+*/
+
+void		check_syntax(t_parts *file)
+{
+	t_parts *token;
+
+	token = file;
+	while (token)
+	{
+		if (token->token >= LIVE && token->token <= AFF)
+			if (!valid_oper_line(&token))
+			{
+				break ;
+			}
+	}
+	// exit if valid_oper_line is 0
+}
