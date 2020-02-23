71d3e7db937069cf174edb4986916b028a6c8fba
Aidan Sullivan <asulliva@f1r2s21.codam.nl>
2020-02-23 14:14:28 +0100

    trying to make it work

---
+++ VM/examples/test_oper.s
@@ -0,0 +1,12 @@
+#by zaz, still not as good as rainbowdash though
+#2013
+
+.name		"test oper"
+.comment	"one two, one two, test test"
+
+	sti		r1, %:label, %1
+	sti		r2, %:label, %5
+label:
+	sti		r10, 12, %3
+	sti		r3, %:label, %10
+	sti		r16, %:label, %15
