460561d944115596cc243965ad1fc579add6c090
Aidan Sullivan <asulliva@f1r6s3.codam.nl>
2020-03-06 14:54:27 +0100

    normed get_next_label.c

--- asm_/get_next_label.c
+++ asm_/get_next_label.c
@@ -6,14 +6,12 @@
 /*   By: abumbier <abumbier@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/25 19:41:35 by abumbier       #+#    #+#                */
-/*   Updated: 2020/02/28 18:30:25 by abumbier      ########   odam.nl         */
+/*   Updated: 2020/03/06 14:54:01 by asulliva      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/asm.h"
 
-/*
-*/
 static int	add_instr_line(t_label *new, t_asm *data, char ***split)
 {
 	set_lines(new, data->lines);
@@ -44,15 +42,17 @@ int			add_parts_to_data(char *str, t_asm *data, t_label *new)
 	else
 	{
 		free_arr(&str, &split, 2);
-		error("Invalid label", data->lines);									// free data
+		error("Invalid label", data->lines);
 	}
 	return (0);
 }
+
 /*
 **	@desc	- function get the label variables if its not on the same line
 **	@param	- t_asm *data, main struct
 **			- char *name, name of the label
 */
+
 void		get_next_label(t_asm *data, char *name)
 {
 	int		last;
@@ -80,50 +80,3 @@ void		get_next_label(t_asm *data, char *name)
 		add_label(data, &new);
 	}
 }
-/*
-
-void		get_next_label(t_asm *data, char *name)
-{
-	char	*str;
-	int		last;
-	char	**split;
-	t_label	*new;
-
-	split = NULL;
-	new = make_label(data, name, -1);
-	last = 1;
-	while (get_line(data, data->rfd, &str, NULL))
-	{
-		if (str && !ft_strequ("", str))
-		{
-			last = 0;
-			split = ft_strsplit_ws(str);
-			if (check_instruction(split[0]))
-			{
-				set_lines(new, data->lines);
-				add_label(data, &new);
-				parse_instruction(data, split);
-				break ;
-			}
-			else if (split[0][ft_strlen(str) - 1] == LABEL_CHAR)
-				add_to_label(data, split[0], &new);
-			else if (ft_strchr(split[0], LABEL_CHAR))
-			{
-				add_label(data, &new);
-				set_lines(new, data->lines);
-				get_label(data, split);
-				break ;
-			}
-			else
-				error("Invalid label", data->lines);									// free data
-		}
-		free_arr(&str, &split, 2);
-	}
-	free_arr(&str, &split, 2);
-	if (last)
-	{
-		set_lines(new, data->lines);
-		add_label(data, &new);
-	}
-}
- */
